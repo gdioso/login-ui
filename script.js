924b174aec420110b68721beb17d14e6f0d1259b
Gerrime Dioso <gdioso98@gmail.com>
2022-12-22 23:01:47 -0800

    commit 3

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggleVisibility = document.querySelector('#toggleVisibility');
+const password = document.querySelector('#password');
+
+toggleVisibility.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});        
